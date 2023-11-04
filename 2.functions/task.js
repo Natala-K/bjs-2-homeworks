d58cd4acc68fadc04473876afb54ad73445efeec
Kolmy <test@neto.com>
2023-11-04 19:05:24 +0300

    Первое задание неполностью готово

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,7 +1,27 @@
 function getArrayParams(...arr) {
 
-  return { min: min, max: max, avg: avg };
-}
+  let min = [1, 2, 3, 4, 5, 6, 7];
+  let max = [-35, 1, 12, 18, -9];
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+    sum = +arr[i];
+
+    if item > max {
+      item == max;
+    }
+   
+    if item < min {
+      item == min;
+    }
+
+   let avr = item + sum 
+    
+   return min
+   return max
+   return avr
+
+  }
 
 function summElementsWorker(...arr) {
 
