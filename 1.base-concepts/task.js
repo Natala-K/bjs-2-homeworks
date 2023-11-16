ec3352e9de38405d56b629824ffbd7e8738a90eb
Kolmy <test@neto.com>
2023-11-17 00:07:05 +0300

    Исправлено

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -4,26 +4,25 @@ function solveEquation(a, b, c) {
 
 	let d = b ** 2 - 4 * a * c
 	let x1, x2 = [];
-	let x = x;
+	let u = x;
 	let arr = [];
-	let x0 == (-b / (2 * a));
+	let x0 = (-b / (2 * a));
 	let x1 = (-b + Math.sqrt(d)) / (2 * a);
 	let x2 = (-b - Math.sqrt(d)) / (2 * a);
 
-	return arr;
-
-
-	if (d == 0) {
-
+	
+    if (d == 0) {
 		arr.push[x0];
 
 	} else if (d > 0) {
 
-		arr.push[x2];
-		arr.push[x1];
+		arr.push(x1, x2);
 
 	}
+
+	return arr;
 }
+
 
 function calculateTotalMortgage(percent, contribution, amount, countMonths) {
 
