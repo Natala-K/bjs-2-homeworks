5d0de3c5e279659df5490fdc950796f667f94cfe
Kolmy <test@neto.com>
2024-01-10 16:47:13 +0300

    added

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -8,16 +8,11 @@ function getArrayParams(...arr) {
   let sum = 0;
 
  for(let i = 0; i < arr.length; i++) {
-  if (arr[i] > max) {
-    max = arr[i]
+  if (arr[i] > max && arr[i] < min) {
+    max = arr[i];
+    min = arr[i];
   } 
-  if (arr[i] < min) {
-    min = arr[i]
-  }
-}
-
-
- 
+  } 
  
 let average = sum(arr) / 3;
 
