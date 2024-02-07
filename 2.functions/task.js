45662ed0d62a6bb19d5db1ba1d3239a62e61f1d9
Kolmy <test@neto.com>
2024-02-07 21:51:48 +0300

    objects

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,47 +1,133 @@
 
-
-
 function getArrayParams(...arr) {
 
   let min = Infinity;
   let max = -Infinity;
   let sum = 0;
+  
 
- for(let i = 0; i < arr.length; i++) {
-  if (arr[i] > max && arr[i] < min) {
-    max = arr[i];
-    min = arr[i];
-  } 
-  } 
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] < min) {
+      min = arr[i];
+    }
+
+    if (arr[i] > max) {
+     max = arr[i]
+    }
+   
+    sum +=arr[i]; 
+  }
  
-let average = sum(arr) / 3;
-
-return { min: min, max: max, average: avg };
+  let avg = Number((sum / arr.length).toFixed(2));
+  return { min: min, max: max, avg: avg };
+  
 }
-
-
-
-
-
-
 
 
 function summElementsWorker(...arr) {
 
+  let sum = 0;
+
+  for (let i = 0; i < arr.length; i++) {
+  sum +=arr[i];
 }
+
+return (sum)
+}
+
+
 
 function differenceMaxMinWorker(...arr) {
 
+  let min = Infinity;
+  let max = -Infinity;
+ 
+
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] < min) {
+      min = arr[i];
+    };
+
+    if (arr[i] > max) {
+      max = arr[i]
+     }
+
+  }
+  if (arr.length == 0) {
+    return 0;
+  }
+
+
+  let difference = max - min;
+  return (difference)
+
 }
+
+
 
 function differenceEvenOddWorker(...arr) {
 
-}
+  let sumEvenElement = 0;
+  let sumOddElement = 0;
+  
+  for (let i = 0; i < arr.length; i++) {
+
+    if (arr[i] % 2==0) {
+      sumEvenElement +=arr[i];     
+    }
+
+    else {
+      sumOddElement +=arr[i];    
+    }
+  
+  }
+  let difference = sumEvenElement - sumOddElement; 
+  return (difference)
+  
+  }
+
 
 function averageEvenElementsWorker(...arr) {
 
+let sumEvenElement = 0;
+let countEvenElement = 0;
+
+for (let i = 0; i < arr.length; i++) {
+
+  if (arr[i] % 2==0) {
+    sumEvenElement +=arr[i]; 
+    countEvenElement +=1;
+  }
+
 }
 
-function makeWork (arrOfArr, func) {
+if (arr.length == 0) {
+  return 0;
+}
+
+ let result = sumEvenElement / countEvenElement;
+ return (result)
+
+ }
+
+
+
+
+ function makeWork(arrOfArr, func) {
+
+  let maxWorkerResult = -Infinity;
+
+  const arr2 = [];
+
+  for (let i = 0; i < arrOfArr.length; i++) {
+  arr2.push(func(arrOfArr[i]));
+}
+ 
+const arr3 = [...arr2];
+
+maxWorkerResult = Math.max(arr3)
+
+return (maxWorkerResult);
 
 }
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,39 @@
 function Student(name, gender, age) {
-  
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
+  this.marks = [];
 }
 
+const newStudent = new Student('Pert', 'male', '19')
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName;
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.hasOwnProperty('marks') && this.marks != []) {
+this.marks.push(...marks);
+  } else {
+    return 0;
+  }
 }
 
 Student.prototype.getAverage = function () {
-  
+if (this.marks == false || this.marks == []) {
+    return 0;
+} else {
+    let length = this.marks.length;
+    let sum = this.marks.reduce((acc, mark) => acc + mark, 0);
+    this.average = sum / length;
+    return sum / length;
+}
 }
 
 Student.prototype.exclude = function (reason) {
-  
+  this.excluded = reason;
+  delete this.subject;
+  delete this.marks;
 }
+
+newStudent.exclude('прогулы');
